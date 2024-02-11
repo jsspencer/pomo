506930223c0fd1065e94b2424330f5a71b66f6a9
Stel Abrego <stel@stel.codes>
2024-02-10 20:47:35 -0800

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A flake for this fork of the Pomo bash pomodoro timer";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.callPackage ./. { };
+      });
+}
